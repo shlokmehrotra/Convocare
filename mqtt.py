d2a6bdc9a44f7c13b40bce6b4b7eff95fec87e34
tiger135 <vishakh.arora29@gmail.com>
2020-01-18 23:21:35 -0800

    Working doctor side programs

---
+++ mqtt.py
@@ -0,0 +1,82 @@
+import sys
+import paho.mqtt.client as mqtt
+import time
+import subprocess
+
+# try:
+#     import RPi.GPIO as GPIO
+# except RuntimeError:
+#     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+TOPIC_PATH="/dvhacks/record/9"
+OFF=0
+ON=1
+CHANNELS = [3,5,7,8,10,11,12,13,15,16,18,19,21,22,23,24,26,29,31,32,33,35,36,37]
+DEBUG = False
+
+def dbg(msg):
+    global DEBUG
+    if DEBUG:
+        print(msg)
+        sys.stdout.flush()
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    dbg("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    dbg("Subscribing to " + TOPIC_PATH)# +"#")
+    client.subscribe( TOPIC_PATH) #+"#")
+
+# Function that abstracts on/off condition for GPIO state
+# def gpioStateFor(on):
+#     if on:
+#         return GPIO.HIGH
+#     return GPIO.LOW
+
+# The callback for when a PUBLISH message is received from the server.
+
+def record_audio():
+    global f_kill
+    f_kill = subprocess.Popen(["python","record_audio.py"])
+
+def stop_record_audio():
+    f_kill.terminate()
+
+def publish_audio():
+    f = open("/home/pi/doctor1.wav","rb")
+    audio = f.read()
+    f.close()
+    audio_actual = bytearray(audio)
+    print("publishing audio")
+    client.publish('/dvhacks/record/10',audio)
+
+
+def on_message(client, userdata, msg):
+    print("received")
+    command = str(msg.payload)
+    #print(command)
+    if (command == ("b\'start\'")):
+        record_audio()
+    if (command == ("b\'stop\'")):
+        stop_record_audio()
+        publish_audio()
+
+if len(sys.argv) > 1 and sys.argv[1] == '-d':
+    DEBUG = True
+client = mqtt.Client()
+
+print("connecting")
+client.connect("mqtt.eclipse.org")
+print("connected")
+client.on_connect = on_connect
+client.on_message = on_message
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setwarnings(False)
+# GPIO.setup(CHANNELS, GPIO.OUT)
+# GPIO.output(CHANNELS, gpioStateFor(OFF))
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
--- server.py
+++ server.py
@@ -7,6 +7,40 @@ app = Flask(__name__)
 
 @app.route('/stop')
 def stop():
-    # mosquitto_pub -d -t omnihacks -m \"record\"
     os.system("python publish.py")
     return render_template("STOP.html")
+
+@app.route('/start')
+def start():
+    os.system("python stopublish.py")
+    print("generating transcript")
+    global blood_pressure, heart_rate
+    # metrics = os.system("python TranscriptGeneration/transcript_create.py")
+    time.sleep(15)
+    metrics = transcript_gen()
+    print(type(metrics), len(metrics))
+    blood_pressure = str(metrics[0])
+    heart_rate = str(metrics[1])
+    print("done generating transcript")
+    print("sending email")
+    send_email()
+    print("sent email")
+    return render_template("START.html")
+
+@app.route('/metrics')
+def metrics():
+    #adafruit.main(0, blood_pressure)
+    return redirect("https://io.adafruit.com/vishakh_arora29/dashboards/dvhacks-metrics?kiosk=true")
+
+@app.route('/transcript')
+def transcript():
+    fin = open("transcript.txt","r")
+    return fin.read()
+
+
+@app.route('/')
+def index():
+    return render_template("START.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
